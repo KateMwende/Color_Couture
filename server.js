be509013bdef8c3b07b4eb2f6144325633b394dc
Kate Mwende <Kmwende419@gmail.com>
2023-07-13 17:31:00 +0300

    changed nodemailer import location

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const nodemailer = require('nodemailer');
 const mysql = require('mysql');
 const app = express();
 const port = 3000;
