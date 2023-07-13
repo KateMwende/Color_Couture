0e492127a427abb5e6646cd528ae1a9a80ca467e
Kate Mwende <Kmwende419@gmail.com>
2023-07-13 17:24:13 +0300

    Imported nodemailer to enable emails

--- routes/submit.js
+++ routes/submit.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
 
 module.exports = (connection) => {
   // Create an appointment (POST route)
