53b6276307a8de8da26c6bfd0be43f0331cfdb88
Kate Mwende <Kmwende419@gmail.com>
2023-10-18 23:40:26 +0300

    changed date to YYYY trying to avoid date issues

--- web_static/script.js
+++ web_static/script.js
@@ -1,46 +1,56 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            customer_name: '',
-            date: '',
-            time: '',
-            service: '',
-            showNotification: false
-        };
+  data() {
+    return {
+      customer_name: '',
+      date: '',
+      time: '',
+      service: '',
+      showNotification: false,
+    };
+  },
+  methods: {
+    handleSubmit(event) {
+      // Check if the selected date is in the past
+      const selectedDate = moment(this.date, 'DD-MM-YYYY');
+      const today = moment();
+
+      if (selectedDate.isBefore(today, 'day')) {
+        // Display an error message to the user
+        this.errorMessage = 'Please select a future date.';
+        return;
+      }
+      event.preventDefault();
+      // Make a POST request to the server
+      axios
+        .post('https://color-couture.onrender.com/submit', {
+          customer_name: this.customer_name,
+          date: moment(this.date, 'DD-MM-YYYY').format('YYYY-MM-DD'),
+          time: this.time,
+          service: this.service,
+          showNotification: false,
+        })
+        .then((response) => {
+          console.log(response.data); // Success message from the server
+          // Display a confirmation message to the user
+          this.showNotification = true;
+          this.notificationMessage = 'Enjoy your date with us!!';
+          // Reset the form
+          this.customer_name = '';
+          this.date = '';
+          this.time = '';
+          this.service = '';
+          // Set a timer to hide the notification after 6 seconds
+          setTimeout(() => {
+            this.showNotification = false;
+            this.notificationMessage = '';
+          }, 6000);
+        })
+        .catch((error) => {
+          console.log(error.response.data); // Error message from the server
+          // Display an error message to the user
+        });
     },
-    methods: {
-        handleSubmit(event) {
-            event.preventDefault();
-            // Make a POST request to the server
-            axios.post('https://color-couture.onrender.com/submit', {
-                customer_name: this.customer_name,
-                date: moment(this.date, 'DD-MM-YY').format('YYYY-MM-DD'),
-                time: this.time,
-                service: this.service,
-                showNotification: false
-            })
-                .then(response => {
-                    console.log(response.data); // Success message from the server
-                    // Display a confirmation message to the user
-                    this.showNotification = true;
-                    this.notificationMessage = "Enjoy your date with us!!";
-                    // Reset the form
-                    this.customer_name = '';
-                    this.date = '';
-                    this.time = '';
-                    this.service = '';
-                    // Set a timer to hide the notification after 6 seconds
-                    setTimeout(() => {
-                        this.showNotification = false;
-                        this.notificationMessage = '';
-                    }, 6000);
-                })
-                .catch(error => {
-                    console.log(error.response.data); // Error message from the server
-                    // Display an error message to the user
-                });
-        }
-    }
+  },
 });
 
 app.mount('#app');
